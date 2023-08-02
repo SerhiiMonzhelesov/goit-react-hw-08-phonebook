895708ab4ac015aa825d96a57263af76bc8672e2
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-08-02 13:25:30 +0300

    prop

--- src/Guards/PrivateRoute.jsx
+++ src/Guards/PrivateRoute.jsx
@@ -1,8 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectToken } from 'redux/selectors';
+import PropTypes from 'prop-types';
 
 export const PrivateRoute = ({ children }) => {
   const token = useSelector(selectToken);
   return token ? children : <Navigate to="/" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
 };
--- src/Guards/PublicRoute.jsx
+++ src/Guards/PublicRoute.jsx
@@ -1,8 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectToken } from 'redux/selectors';
+import PropTypes from 'prop-types';
 
 export const PublicRoute = ({ children }) => {
   const token = useSelector(selectToken);
   return !token ? children : <Navigate to="/contacts" />;
+};
+
+PublicRoute.propTypes = {
+  children: PropTypes.node,
 };
